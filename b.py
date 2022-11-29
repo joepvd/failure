280ee664bf7ea05fd407e372aa7fbcee74dd5054
Joep van Delft <jpvndlft@gmail.com>
2022-11-29 17:50:50 +0100

    initial_green

---
+++ b.py
@@ -0,0 +1,6 @@
+import a
+def g():
+    return a.f() + 1
+
+if __name__ == '__main__':
+    print(g())
---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+import a
+import b
+
+class TestThing(unittest.TestCase):
+    def test_a(self):
+        self.assertEqual(a.f(), 1)
+
+    def test_b(self):
+        self.assertEqual(b.g(), 2)
+
+if __name__ == '__main__':
+    unittest.main()
